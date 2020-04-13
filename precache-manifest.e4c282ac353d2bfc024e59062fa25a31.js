41ffcb386597d9e52a9e62781e4527b49233c1db
rafaelbeckert <rafaelbeckert>
2020-04-12 22:37:55 -0300

    Updates

---
+++ precache-manifest.e4c282ac353d2bfc024e59062fa25a31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d813c3e45974366bcaf3a5aca899ab96",
+    "url": "/t2/index.html"
+  },
+  {
+    "revision": "0742e3c1ceb0fa734c5f",
+    "url": "/t2/static/css/main.75677ba3.chunk.css"
+  },
+  {
+    "revision": "a953a4d406266eeddb92",
+    "url": "/t2/static/js/2.4a35e47f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/t2/static/js/2.4a35e47f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0742e3c1ceb0fa734c5f",
+    "url": "/t2/static/js/main.1c363962.chunk.js"
+  },
+  {
+    "revision": "efa657b8de1c4e4d670e",
+    "url": "/t2/static/js/runtime-main.aba49ba1.js"
+  }
+]);
